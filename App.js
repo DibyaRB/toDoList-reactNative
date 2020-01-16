174a270166bfcd94550aa6775f03aca7acc2e052
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2020-01-16 15:20:57 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import Header from './components/header';
+
+export default function App() {
+
+
+  const [todos,setTodos] = useState([
+    {
+      text: 'buy coffee', key: '1'
+    },
+    {
+      text: 'create an app', key:'2'
+    },
+    {
+      text: 'play on the PC', key:'3'
+    }
+  ]);
+  return (
+    <View style={styles.container}>
+    <Header/>
+    {/* header */}
+    <View style={styles.content}>
+
+      <View style={styles.list}>
+      <FlatList
+      data={todos} 
+      renderItem={({item})=>(
+
+        <Text> {item.text} </Text>
+        
+      )}
+
+      />
+
+
+      </View>
+
+
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+    
+  },
+  content:{
+    padding:40,
+  },
+  list:{
+    marginTop:20
+  }
+});
